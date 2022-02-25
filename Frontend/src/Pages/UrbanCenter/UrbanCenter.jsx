2680eb2b357f5ee3b6d7fe5268a69805ceead6aa
Redouane Bouabana <rredouane342@gmail.com>
2022-02-25 22:02:08 +0100

    urban center  management dashboard

---
+++ Frontend/src/Pages/UrbanCenter/UrbanCenter.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import Modal from "../../components/Modals"
+import UrbanCenterTable from "../../components/UrbanCenterTable"
+const UrbanCenter = () => {
+    const [open , setIsOpen]  = useState(false)
+return (
+    <div className="w-[90%] mx-auto">
+    <div className="flex justify-between items-center">
+      <h1 className="text-4xl font-black  py-14"> Manage Urban Center </h1>
+      <button
+        onClick={() => setIsOpen(!open)}
+        type="button"
+        className="h-12 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Add Managers
+      </button>
+    </div>
+    <UrbanCenterTable />
+    <Modal
+      isOpen={open}
+      setIsOpen={setIsOpen}
+    //   component={<AddManagerForm isOpen={open} setIsOpen={setIsOpen} />}
+    />
+  </div>
+)
+}
+export default UrbanCenter
--- Frontend/src/routes/routes.jsx
+++ Frontend/src/routes/routes.jsx
@@ -3,6 +3,7 @@ import UserVaccine from "../Pages/UserVaccine";
 import AdminLogin from "../Pages/AdminLogin";
 import AdminDash from "../Pages/AdminDash";
 import ManagerLogin from "../Pages/ManagerLogin";
+import UrbanCenter from "../Pages/UrbanCenter/UrbanCenter";
 const Routers = () => {
   return (
     <BrowserRouter>
@@ -12,6 +13,7 @@ const Routers = () => {
           <Route path="/managerLogin" element={<ManagerLogin />} />
           <Route path="/" element={<UserVaccine />} />
           <Route path="/AdminDash" element={<AdminDash />} />
+          <Route path="/urbanCenter" element={<UrbanCenter />} />
         </Routes>
       </div>
     </BrowserRouter>
