6f6ca03dec1c250a3918310ecc26a572ab043f50
Redouane Bouabana <rredouane342@gmail.com>
2022-03-06 18:11:23 +0100

    epic.

--- Frontend/src/Pages/AdminDash/index.jsx
+++ Frontend/src/Pages/AdminDash/index.jsx
@@ -2,15 +2,14 @@ import ManagerTable from "../../components/ManagerTable";
 import Modal from "../../components/Modals";
 import { useState } from "react";
 import AddManagerForm from "../../components/Forms/AddManagerForm";
-import StatAdmin from '../../components/Statistiques/StatAdmin'
-
+import StatAdmin from "../../components/Statistiques/StatAdmin";
 
 const AdminDash = () => {
   const [open, setIsOpen] = useState(false);
 
   return (
-    <div className="w-[100%]">
-      <div className="flex justify-between items-center ">
+    <div className="w-[90%] mx-auto">
+      {/* <div className="flex justify-between items-center ">
         <h1 className="text-4xl font-black pl-12  py-14">Managers</h1>
         <button
           onClick={() => setIsOpen(!open)}
@@ -19,23 +18,31 @@ const AdminDash = () => {
         >
           Add Managers
         </button>
-      </div>
-      <div className="flex">
-        <div className="w-full">
-        <ManagerTable />
+      </div> */}
+      <div className="flex justify-between">
+        <div className="w-[70%]">
+          <div className="flex justify-between items-center w-[98%]">
+            <h1 className="text-4xl font-black  py-14"> Managers </h1>
+            <button
+              onClick={() => setIsOpen(!open)}
+              type="button"
+              className="h-12 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Add Manager
+            </button>
+          </div>
+          <ManagerTable />
         </div>
-
-        <StatAdmin 
-          title={"Manager per region"}
-          role={"managers"}
-        />
-       </div>
-       <Modal
-          isOpen={open}
-          title="add Manager"
-          setIsOpen={setIsOpen}
-          component={<AddManagerForm isOpen={open} setIsOpen={setIsOpen} />}
-        />
+        <div className="w-[30%] py-14 ">
+          <StatAdmin title={"Manager per region"} role={"managers"} />
+        </div>
+      </div>
+      <Modal
+        isOpen={open}
+        title="add Manager"
+        setIsOpen={setIsOpen}
+        component={<AddManagerForm isOpen={open} setIsOpen={setIsOpen} />}
+      />
     </div>
   );
 };
--- Frontend/src/Pages/UrbanCenter/UrbanCenter.jsx
+++ Frontend/src/Pages/UrbanCenter/UrbanCenter.jsx
@@ -1,50 +1,53 @@
-import { useState } from "react"
-import AddUrbanCenterForm from "../../components/Forms/AddUrbanCenter"
-import Modal from "../../components/Modals"
-import UrbanCenterTable from "../../components/UrbanCenterTable"
-import StatManager from '../../components/Statistiques/StatManager'
+import { useState } from "react";
+import AddUrbanCenterForm from "../../components/Forms/AddUrbanCenter";
+import Modal from "../../components/Modals";
+import UrbanCenterTable from "../../components/UrbanCenterTable";
+import StatManager from "../../components/Statistiques/StatManager";
 import { useQuery } from "react-query";
 import axios from "axios";
 
 const UrbanCenter = () => {
-    const [open , setIsOpen]  = useState(false)
-      // Queries
-      const query = useQuery("urbanCenter", async () => {
-        const { data } = await axios.get("http://localhost:4000/api/urbanCenter");
-        return data;
-      });
-      
-    return (
-        <div className="w-[90%] mx-auto">
-        <div className="flex justify-between items-center">
-          <h1 className="text-4xl font-black  py-14"> Manage Urban Center </h1>
-          <button
-            onClick={() => setIsOpen(!open)}
-            type="button"
-            className="h-12 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Add UrbanCenter
-          </button>
+  const [open, setIsOpen] = useState(false);
+  // Queries
+  const query = useQuery("urbanCenter", async () => {
+    const { data } = await axios.get("http://localhost:4000/api/urbanCenter");
+    return data;
+  });
+
+  return (
+    <div className="w-[90%] mx-auto">
+      <div className="flex justify-between">
+        <div className="w-[70%]">
+          <div className="flex justify-between items-center w-[98%]">
+            <h1 className="text-4xl font-black  py-14">
+              {" "}
+              Manage Urban Center{" "}
+            </h1>
+            <button
+              onClick={() => setIsOpen(!open)}
+              type="button"
+              className="h-12 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Add UrbanCenter
+            </button>
+          </div>
+          <UrbanCenterTable />
         </div>
-        <div className="flex">
-        <div className="w-full">
-        <UrbanCenterTable />
+        <div className="w-[30] py-14">
+          <StatManager
+            role={"urbanCenter"}
+            StatistiqueTitle={"Vaccin per region"}
+          />
         </div>
-        <StatManager 
-            role = {'urbanCenter'}
-            StatistiqueTitle = {'Vaccin per region'}
-        />
-       </div>
-
-
-
-        <Modal
-          isOpen={open}
-          title={"Add Urban Center"}
-          setIsOpen={setIsOpen}
-          component={<AddUrbanCenterForm setIsOpen={setIsOpen} isOpen={open} /> }
-        />
       </div>
-    )
-}
-export default UrbanCenter
+
+      <Modal
+        isOpen={open}
+        title={"Add Urban Center"}
+        setIsOpen={setIsOpen}
+        component={<AddUrbanCenterForm setIsOpen={setIsOpen} isOpen={open} />}
+      />
+    </div>
+  );
+};
+export default UrbanCenter;
