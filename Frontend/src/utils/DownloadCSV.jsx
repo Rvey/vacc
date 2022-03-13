65911ef10bde3e118944a6fcef0cd2a6b0f4e7d8
Redouane Bouabana <rredouane342@gmail.com>
2022-03-13 19:28:55 +0100

    Update DownloadCSV.jsx

--- Frontend/src/utils/DownloadCSV.jsx
+++ Frontend/src/utils/DownloadCSV.jsx
@@ -21,7 +21,9 @@ const DownloadCSV = () => {
           data={query.data}
           filename={"tableData"}
         >
-          <button className="h-12 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Download full list</button>
+          <button className="h-12 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Download full list
+          </button>
         </CSVDownloader>
       </label>
     </div>
